9cf4c2626b66f1a52770d44a6ceb3160498d377a
unknown <pmmilamiv@gmail.com>
2018-02-17 19:23:25 -0600

    created and tested the letter and word docs

---
+++ Letter.js
@@ -0,0 +1,25 @@
+var Letter = function (letter){
+	this.character = letter;
+	this.guessed = false;
+	this.hideShow = function () {
+		if (this.guessed) {
+			return this.character
+		} else {
+			return "_"
+		}
+	};
+	this.update = function (guess) {
+		if (guess == this.character) {
+			this.guessed = true;
+		}
+	}
+};
+
+
+//******TEST ZONE*********
+// var chips = process.argv[2];
+// var test = new Letter("w");
+// console.log(test);
+// console.log(test.hideShow(chips));
+
+module.exports = Letter;
---
+++ Word.js
@@ -0,0 +1,34 @@
+var Letter = require('./Letter.js');
+// var guess = process.argv[2];
+
+var Word = function (b){
+	// each letter of the word is in this array (wet version)
+	this.word = b.split('');
+	//each letter obj is in this array
+	this.objArray = [];
+	//the word inProgress is in this array (this will be printed to the console)
+	this.inProg = [];
+	this.create = function () {
+		for (i=0;i<this.word.length;i++) {
+			var snarf = new Letter(this.word[i]);
+			this.objArray.push(snarf);
+		}
+	};
+	this.check = function (j) {
+		this.inProg = [];
+		for (i=0;i<this.objArray.length;i++) {
+			//need to define guess earlier
+			this.objArray[i].update(j);
+			this.inProg.push(this.objArray[i].hideShow());
+		}
+	};
+}
+
+var test = new Word("testing");
+test.create();
+test.check(process.argv[2]);
+console.log(test.inProg);
+test.check(process.argv[3]);
+console.log(test.inProg);
+test.check(process.argv[4]);
+console.log(test.inProg);
